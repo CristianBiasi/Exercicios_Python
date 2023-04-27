6e35080e45cb9bdb4f19c7afba518633a3b9e9b3
CristianBiasi <131414109+CristianBiasi@users.noreply.github.com>
2023-04-27 18:28:23 -0300

    Delete exercicios_python/exercicios directory

--- exercicios_python/exercicios/12.py
+++
@@ -1,6 +0,0 @@
-#A Loja Mamão com Açúcar está vendendo seus produtos em 5 (cinco) prestações sem juros. Faça um algoritmo que receba um valor de uma compra e mostre o valor das prestações.
-produto = float(input('Digite o valor do produto: '))
-prestaçoes = int(input('Digite a quantidade de prestacoes: '))
-compra = produto / 5
-
-print('Voce adquiriu o produto X no valor de {}, e dividiu em um total de {} prestacoes, e cada prestacao ficou no valor de {}. '.format(produto,prestaçoes,compra))
